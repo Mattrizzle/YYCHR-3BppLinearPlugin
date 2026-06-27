bb04944763f29eea0c2c1df894c52f035f790bb5
Mattrizzle <m.grassman87@gmail.com>
2026-06-27 19:07:46 -0400

    Added files

---
+++ README.md
@@ -0,0 +1,5 @@
+# YYCHR-3BppLinear
+Plugin for the .NET versions of YY-CHR to handle the 3bpp linear format used by the rotating Mode 7 background elements of the Iggy, Larry and Reznor battles in Super Mario World.
+This is based on the [YY-CHR SNES 8bpp planar plugin by mrehkopf a.k.a. ikari_01](https://github.com/mrehkopf/YYCHR-Snes8BppPlanar) and the [CGA 2bpp linear plugin by gzip](https://github.com/gzip/cs-yychr-plugins), which are derivatives of YY's YY-CHR.NET sample plugin source files.
+
+To compile, place a copy of CharactorLib.dll from YY-CHR into the same directory as YYCHR3BppLinear.cs, open the YYCHR3BppLinear.sln solution file in Visual Studio, then go to the Build menu > Build Solution. Copy the 3BppLinear.dll file from the bin/Debug folder and paste it into YY-CHR's plugins directory. Build tested in Visual Studio Community 2026.
---
+++ YYCHR3BppLinear.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using CharactorLib.Common;
+using CharactorLib.Format;
+using System.Drawing;
+
+namespace YYCHR3BppLinear
+{
+    public class _3BppLinearFormat : FormatBase
+    {
+        public _3BppLinearFormat()
+        {
+            base.FormatText = "[8][8]";
+            base.Name = "3BPP Linear (SMW Mode 7)";
+            base.Extension = "smc,sfc,fig";
+            base.Author = "Mattrizzle";
+            base.Url = "https://github.com/Mattrizzle/YYCHR-3BppLinearPlugin";
+            // Flags
+            base.Readonly = false;
+            base.IsCompressed = false;
+            base.EnableAdf = true;
+            base.IsSupportMirror = false;
+            base.IsSupportRotate = false;    
+            // Settings
+            base.ColorBit = 3;
+            base.ColorNum = 8;
+            base.CharWidth = 8;
+            base.CharHeight = 8;
+            // Settings for Image Convert
+            base.Width = 128;
+            base.Height = 128;
+        }
+
+        // data: contains image data in source format (ROM)
+        // addr: contains source address
+        // bytemap: contains image data in YYCHR internal format
+        // px: contains x coordinate of current pixel
+        // py: contains y coordinate of current pixel
+
+        /* convert from source format (ROM) to YYCHR graphics, one tile at a time */
+        public override void ConvertMemToChr(Byte[] data, int addr, Bytemap bytemap, int px, int py)
+        {
+            for (int x = 0; x < CharWidth; x++)
+            {
+                for (int y = 0; y < CharHeight; y++)
+                {
+                    int pixel = 0x00;
+                    int byteIndex = y * 3;
+                    int byte0 = data[addr + byteIndex];
+                    int byte1 = data[addr + byteIndex + 1];
+                    int byte2 = data[addr + byteIndex + 2];
+                    int b = ( byte0 << 16) ^ ( byte1 << 8 ) ^ byte2;
+
+                    /* get pixel address for requested coordinates in YYCHR bitmap */
+                    Point p = base.GetAdvancePixelPoint(px + x, py + y);
+                    int bytemapAddr = bytemap.GetPointAddress(p.X, p.Y);
+
+                    // gather bits comprising pixel
+                    pixel = b >> 3 * (7 - (x % 8)) & 7;
+
+                    /* write pixel to YYCHR bitmap */
+                    bytemap.Data[bytemapAddr] = (byte) pixel;
+                }
+            }
+        }
+
+        /* convert from YYCHR graphics to source format (ROM), one tile at a time */
+        public override void ConvertChrToMem(Byte[] data, int addr, Bytemap bytemap, int px, int py)
+        {
+            for (int x = 0; x < CharWidth; x++)
+            {
+                for (int y = 0; y < CharHeight; y++)
+                {
+                    // get pixel from YYCHR bitmap
+                    Point p = base.GetAdvancePixelPoint(px + x, py + y);
+                    int bytemapAddr = bytemap.GetPointAddress(p.X, p.Y);
+                    int pixel = bytemap.Data[bytemapAddr];
+
+                    // calculate the byte index for this pixel
+                    int byteIndex = y * 3;
+
+                    // start fresh if it's a new byte, otherwise grab the existing data
+                    int byte0 = data[addr + byteIndex];
+                    int byte1 = data[addr + byteIndex + 1];
+                    int byte2 = data[addr + byteIndex + 2];
+                    int b = x % 8 == 0 ? 0x00 : (byte0 << 16) ^ (byte1 << 8) ^ byte2;
+
+                    b |= pixel << 3 * (7 - (x % 8));
+
+                    // write pixel to YYCHR bytemap
+                    data[addr + byteIndex] = (byte)((b >> 16) & 0xFF);
+                    data[addr + byteIndex + 1] = (byte)((b >> 8) & 0xFF);
+                    data[addr + byteIndex + 2] = (byte)(b & 0xFF);
+                }
+            }
+        }
+    }
+}
